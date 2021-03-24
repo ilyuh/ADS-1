78259baf560cc0dc8a93423386473120b4683c06
Ilya Fedotov <iluyha.uho@gmail.com>
2021-03-24 23:53:01 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,29 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+  int left = 0;
+  int right = size - 1;
+  int counter = 0;
+
+  while (left < right) {
+    int mid = (left + right) / 2;
+    if (arr[mid] < value) {
+      left = mid + 1;
+    } else {
+      right = mid;
+    }
+  }
+
+  if (arr[left] == value) {
+    while (arr[left] == value) {
+      counter++;
+      left++;
+    }
+  }
+
+  if (counter != 0) {
+    return counter;
+  } else {
+    return 0;
+  }
 }
